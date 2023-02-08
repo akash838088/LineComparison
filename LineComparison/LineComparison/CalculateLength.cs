5604f2f4141d85e4b9ec7390fe6aefebfeda9020
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-08 22:21:34 +0530

    [Akash] add : UC1

--- LineComparison/LineComparison/CalculateLength.cs
+++ LineComparison/LineComparison/CalculateLength.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LineComparison
+namespace LineComparesion
 {
-    internal class CalculateLength
+    public class CalculateLength
     {
+        int lengthX, _lengthY, lenGthsX, lengthsY;
+        public CalculateLength(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;
+            _lengthY = lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double Calculate()
+        {
+            //Line = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2)
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine(length);
+            return length;
+
+
+        }
     }
 }
